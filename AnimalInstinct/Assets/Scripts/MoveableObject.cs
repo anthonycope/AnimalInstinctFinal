619bb79c14d6334219fd12192132fb2857baa8dc
Roel Palloj <rpalloj@gmail.com>
2016-01-30 13:29:51 -0500

    Moveable object script

---
+++ AnimalInstinct/Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveableObject : MonoBehaviour {
+
+	Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag.Equals ("Player")) {
+			if (other.GetComponent<Character> ().type == CharacterType.Dog) {
+				myRigidBody.constraints = RigidbodyConstraints2D.None;
+
+			} else {
+				myRigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+
+			}
+		}
+	}
+}
