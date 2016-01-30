a5e423fcf88fff628ffcff9833b39322f6db87fb
Ant <ronburgandy91@gmail.com>
2016-01-30 11:33:39 -0500

    Added initial main menu + splash screen
    Main Menu + Intro Animation logic.

---
+++ AnimalInstinct/Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    CanvasGroup mainMenuCanvas;
+    CanvasGroup introAnimationCanvas;
+
+    GameObject levelOne;
+
+    public bool atMainMenu;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+        mainMenuCanvas= GameObject.Find("MainMenuCanvas").GetComponent<CanvasGroup>();
+        introAnimationCanvas = GameObject.Find("IntroCanvas").GetComponent<CanvasGroup>();
+        levelOne = GameObject.Find("LevelOne");
+        
+
+        showMainMenu();
+    }
+
+    private void showMainMenu()
+    {
+        levelOne.SetActive(false);
+
+        introAnimationCanvas.alpha = 0f;
+        introAnimationCanvas.interactable = false;
+        introAnimationCanvas.blocksRaycasts = false;
+
+        mainMenuCanvas.alpha = 1f;
+        mainMenuCanvas.interactable = true;
+        mainMenuCanvas.blocksRaycasts = true;
+
+        atMainMenu = true;
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        CheckInput();
+	
+	}
+
+    private void CheckInput()
+    {
+        if(atMainMenu)
+        {
+
+            if(Input.anyKeyDown)
+            {
+                atMainMenu = false;
+                StartCoroutine(LaunchSplash());
+            }
+        }
+    }
+
+    IEnumerator LaunchSplash()
+    {
+        mainMenuCanvas.alpha = 0f;
+        mainMenuCanvas.interactable = false;
+        mainMenuCanvas.blocksRaycasts = false;
+
+        introAnimationCanvas.alpha = 1f;
+        introAnimationCanvas.interactable = true;
+        introAnimationCanvas.blocksRaycasts = true;
+
+        yield return new WaitForSeconds(5f);
+
+        StartGame();
+
+    }
+
+    private void StartGame()
+    {
+        introAnimationCanvas.alpha = 0f;
+        introAnimationCanvas.interactable = false;
+        introAnimationCanvas.blocksRaycasts = false;
+
+        levelOne.SetActive(true);
+    }
+}
