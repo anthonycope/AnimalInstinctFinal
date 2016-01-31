59bb4039d6973f3e4c28fba28708e10b51cac31b
Ant <ronburgandy91@gmail.com>
2016-01-30 19:14:52 -0500

    Added dog cage functionality.

---
+++ AnimalInstinct/Assets/Scripts/DogCage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DogCage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            //free dog
+            GameObject.Find("DogButton").GetComponent<Button>().interactable = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
