fb1f663288d85f571062e000053ac0dbf591e680
Roel Palloj <rpalloj@gmail.com>
2016-01-31 14:33:24 -0500

    Hiding of waypoints

---
+++ AnimalInstinct/Assets/Scripts/HideWaypoints.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideWaypoints : MonoBehaviour {
+
+
+	private Transform[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+		waypoints = gameObject.GetComponentsInChildren<Transform> ();
+
+		foreach (Transform temp in waypoints) {
+			if (temp.name.Equals ("Waypoint") || temp.name.Equals ("Waypoint 2")) {
+				temp.GetComponent<Renderer> ().material.color = new Color(0, 0, 0, 0);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
